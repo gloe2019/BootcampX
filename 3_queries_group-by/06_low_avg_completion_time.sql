2b97880c4d5e971b7e4622e8337006284dfaf7ae
Gloria Edem <gloedem@gmail.com>
2021-08-16 22:54:57 +0000

    complete round 3 of queries with GROUP BY/HAVING

---
+++ 3_queries_group-by/06_low_avg_completion_time.sql
@@ -0,0 +1,9 @@
+SELECT students.name AS student, AVG(assignment_submissions.duration) AS average_assignment_duration, AVG(assignments.duration) AS average_estimated_duration 
+FROM assignments 
+JOIN assignment_submissions ON assignments.id=assignment_id 
+JOIN students ON student_id=students.id 
+WHERE students.end_date IS NULL 
+GROUP BY student 
+HAVING AVG(assignments.duration) > AVG(assignment_submissions.duration) 
+ORDER BY average_assignment_duration ASC;
+
