20fe69ed6669f2802a4208787dd2d5bf0c67ee3c
Gloria Edem <gloedem@gmail.com>
2021-08-16 18:05:34 +0000

    complete db queries with JOINs

---
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -0,0 +1,1 @@
+SELECT SUM(duration) AS total_duration FROM assignment_submissions JOIN students ON student_id=students.id WHERE student_id IN (SELECT students.id FROM students JOIN cohorts ON cohort_id=cohorts.id WHERE cohorts.name = 'FEB12');
