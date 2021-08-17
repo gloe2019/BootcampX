1c8a7c2452f06a2f7f18bd62a785ddba3f8b05b0
Gloria Edem <gloedem@gmail.com>
2021-08-17 23:46:51 +0000

    completed round 4 queries

---
+++ 4_queries/1_total_teacher_assistance_requests.sql
@@ -0,0 +1,6 @@
+SELECT count(assistance_requests.*) AS total_assistances, teachers.name AS name 
+FROM assistance_requests 
+JOIN teachers on teachers.id=teacher_id 
+WHERE name='Waylon Boehm' 
+GROUP BY teachers.name;
+
